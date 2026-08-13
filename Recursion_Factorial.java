2a23cccc3cde93dd5fed4c0441938439e0e58f20
Tamannamangla <manglastamanna@gmail.com>
2026-08-13 17:44:38 +0530

    Implement recursive factorial function

---
+++ Recursion_Factorial.java
@@ -0,0 +1,12 @@
+class Factorial{
+  public static void main(String[] args){
+    fact(n);
+  }
+  public static int fact(int n){
+    if(n==1){
+      return 1;
+    }
+    int f1 = fact(n-1);
+    return n*f1;
+  }
+}
