40d446126c34ac02c4e9a4bee52fae026243ff14
Tamannamangla <manglastamanna@gmail.com>
2026-08-17 17:44:39 +0530

    Create LinkedListPrint.java
    printing elements in java

---
+++ LinkedListPrint.java
@@ -0,0 +1,34 @@
+public class LinkedListPrint {
+
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static void printList(Node head) {
+
+        Node temp = head;
+
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+
+        Node head = new Node(10);
+        head.next = new Node(20);
+        head.next.next = new Node(30);
+        head.next.next.next = new Node(40);
+
+        printList(head);
+    }
+}
